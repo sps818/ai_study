b91af42273c3d489647298aa1a2ddf0691a03278
sps818 <63447407@qq.com>
2025-05-07 23:06:56 +0800

    modify day08

--- day08/codes/opencv_image_demo.py
+++ day08/codes/opencv_image_demo.py
@@ -1,14 +1,13 @@
 import cv2
 
 # 读取图像
-img = cv2.imread("girl.jpg")
+img = cv2.imread("./day08/codes/cat.jpg")
 
 # 显示图像
-cv2.imshow(winname="girl", mat=img)
+cv2.imshow(winname="cat", mat=img)
 
 # 按键等待
 cv2.waitKey(delay=0)
-
 
 # 释放资源
 cv2.destroyAllWindows()
--- day08/codes/opencv_video_demo.py
+++ day08/codes/opencv_video_demo.py
@@ -1,8 +1,11 @@
 # 引入 OpenCV
 import cv2
 
-# 1. 建立连接
+# 1. 建立连接，0代表电脑上插的第0个摄像头
 cap = cv2.VideoCapture(0)
+
+# 查看是否插了摄像头
+print(f'摄像头是否插了：{cap.isOpened()}')
 
 # 2. 增删改查
 while True:
@@ -10,19 +13,19 @@ while True:
     status, frame = cap.read()
     # 读取失败
     if not status:
-        print('error')
+        print('未能读取到视频帧！')
         break
     # 读取成功
-    # 添加处理代码
-        
+    ###### 添加处理代码 开始########
+
+    ###### 添加处理代码 结束########
+
     # 图像显示
     cv2.imshow(winname="demo", mat=frame)
-    # 暂留一帧（按ESC键退出）
+    # 暂留一帧（按ESC键退出， ESC的ASCII码为27）； 1000 // 24 代表每秒24帧，1000 // 24 代表每帧的暂留时间
     if cv2.waitKey(delay=1000 // 24) == 27:
         break
 
 # 3. 释放资源
 cap.release()
 cv2.destroyAllWindows()
-
-    
