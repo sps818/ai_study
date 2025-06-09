d0edefcb701b423834acda769b69bf0a77e94ef7
sps818 <63447407@qq.com>
2025-06-09 13:46:23 +0800

    增加D14

--- 53qi/D9/02.emo_reco/config/emotion_config.py
+++ 53qi/D9/02.emo_reco/config/emotion_config.py
@@ -1,11 +1,15 @@
 # import the necessary packages
-from os import path
+import os
+
+BASE_PATH = os.path.join(os.getcwd(), "datasets", "fer2013")
+INPUT_PATH = os.path.join(BASE_PATH, "fer2013.csv")
+OUTPUT_PATH = os.path.join(BASE_PATH, "output")
 
 # define the base path to the emotion dataset
-BASE_PATH = "../datasets/fer2013"
+# BASE_PATH = "../datasets/fer2013"
 
 # use the base path to define the path to the input emotions file
-INPUT_PATH = path.sep.join([BASE_PATH, "fer2013/fer2013.csv"])
+# INPUT_PATH = path.sep.join([BASE_PATH, "fer2013/fer2013.csv"])
 
 # define the number of classes (set to 6 if you are ignoring the
 # "disgust" class)
@@ -14,12 +18,12 @@ NUM_CLASSES = 6
 
 # define the path to the output training, validation, and testing
 # HDF5 files
-TRAIN_HDF5 = path.sep.join([BASE_PATH, "hdf5/train.hdf5"])
-VAL_HDF5 = path.sep.join([BASE_PATH, "hdf5/val.hdf5"])
-TEST_HDF5 = path.sep.join([BASE_PATH, "hdf5/test.hdf5"])
+TRAIN_HDF5 = os.path.sep.join([BASE_PATH, "hdf5/train.hdf5"])
+VAL_HDF5 = os.path.sep.join([BASE_PATH, "hdf5/val.hdf5"])
+TEST_HDF5 = os.path.sep.join([BASE_PATH, "hdf5/test.hdf5"])
 
 # define the batch size
 BATCH_SIZE = 128
 
 # define the path to where output logs will be stored
-OUTPUT_PATH = path.sep.join([BASE_PATH, "output"])
+# OUTPUT_PATH = path.sep.join([BASE_PATH, "output"])
